100eb77fcbda412f60c276c7eb32af1da228280d
MM dela Cruz <delacruz.mithimalaya@gmail.com>
2017-06-16 02:26:12 +0800

    about us section
    updated the layout

--- js/custom.js
+++ js/custom.js
@@ -164,6 +164,38 @@ jQuery(document).ready(function ($) {
     });
 });
 
+jssor_1_slider_init = function() {
+	var jssor_1_options = {
+	  $AutoPlay: 1,
+	  $Idle: 0,
+	  $SlideDuration: 5000,
+	  $SlideEasing: $Jease$.$Linear,
+	  $PauseOnHover: 4,
+	  $SlideWidth: 200,
+	  $Cols: 7
+	};
+
+	var jssor_1_slider = new $JssorSlider$("jssor_1", jssor_1_options);
+
+	/*responsive code begin*/
+	/*remove responsive code if you don't want the slider scales while window resizing*/
+	function ScaleSlider() {
+		var refSize = jssor_1_slider.$Elmt.parentNode.clientWidth;
+		if (refSize) {
+			refSize = Math.min(refSize, 980);
+			jssor_1_slider.$ScaleWidth(refSize);
+		}
+		else {
+			window.setTimeout(ScaleSlider, 30);
+		}
+	}
+	ScaleSlider();
+	$Jssor$.$AddEvent(window, "load", ScaleSlider);
+	$Jssor$.$AddEvent(window, "resize", ScaleSlider);
+	$Jssor$.$AddEvent(window, "orientationchange", ScaleSlider);
+	/*responsive code end*/
+};
+		
 var demo1Played = false, demo2Played = false;
 $(document).ready(function () {
 
@@ -239,6 +271,8 @@ $(document).ready(function () {
 			scrollTop: that.offset().top + offset
 		}, 1000);
 	});
+	
+	jssor_1_slider_init();
 });
 
 $(window).load(function() { //start after HTML, images have loaded
